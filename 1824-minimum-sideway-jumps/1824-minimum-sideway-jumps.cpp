b40a618c88f22c760d5f719b2dd36e6303cc64b4
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2024-07-15 16:51:12 +0530

    Time: 187 ms (81.84%), Space: 232.5 MB (63.15%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -27,54 +27,54 @@ public:
         // vector<vector<int>> dp(4, vector<int>(n, -1));
         // return solve(obstacles, 2, 0, dp);
         
-        // vector<vector<int>> dp(4, vector<int>(obstacles.size(), INT_MAX));
-        // dp[0][n] = 0;
-        // dp[1][n] = 0;
-        // dp[2][n] = 0;
-        // dp[3][n] = 0;
-
-        // for(int currPos = n-1; currPos>=0; currPos--){
-        //     for(int currLane = 1; currLane<=3; currLane++){
-        //         if(obstacles[currPos+1] != currLane){
-        //             dp[currLane][currPos] = dp[currLane][currPos+1];
-        //         }
-        //         else{
-        //             int ans = INT_MAX;
-        //             for(int i=1; i<=3; i++){
-        //                 if(currLane != i && obstacles[currPos] != i){
-        //                     ans = min(ans, 1 + dp[i][currPos+1]);
-        //                 }
-        //             }
-        //             dp[currLane][currPos] = ans;
-        //         }
-        //     }
-        // }
-        // return min(dp[2][0], min(1+dp[3][0], 1+dp[1][0]));
-
-        vector<int> curr(4, INT_MAX);
-        vector<int> next(4, INT_MAX);
-        next[0] = 0;
-        next[1] = 0;
-        next[2] = 0;
-        next[3] = 0;
+        vector<vector<int>> dp(4, vector<int>(obstacles.size(), INT_MAX));
+        dp[0][n] = 0;
+        dp[1][n] = 0;
+        dp[2][n] = 0;
+        dp[3][n] = 0;
 
         for(int currPos = n-1; currPos>=0; currPos--){
             for(int currLane = 1; currLane<=3; currLane++){
                 if(obstacles[currPos+1] != currLane){
-                    curr[currLane] = next[currLane];
+                    dp[currLane][currPos] = dp[currLane][currPos+1];
                 }
                 else{
                     int ans = INT_MAX;
                     for(int i=1; i<=3; i++){
                         if(currLane != i && obstacles[currPos] != i){
-                            ans = min(ans, 1 + next[i]);
+                            ans = min(ans, 1 + dp[i][currPos+1]);
                         }
                     }
-                    curr[currLane] = ans;
+                    dp[currLane][currPos] = ans;
                 }
             }
-            next = curr;
         }
-        return min(next[2], min(1+next[1], 1+next[3]));
+        return min(dp[2][0], min(1+dp[3][0], 1+dp[1][0]));
+
+//         vector<int> curr(4, INT_MAX);
+//         vector<int> next(4, INT_MAX);
+//         next[0] = 0;
+//         next[1] = 0;
+//         next[2] = 0;
+//         next[3] = 0;
+
+//         for(int currPos = n-1; currPos>=0; currPos--){
+//             for(int currLane = 1; currLane<=3; currLane++){
+//                 if(obstacles[currPos+1] != currLane){
+//                     curr[currLane] = next[currLane];
+//                 }
+//                 else{
+//                     int ans = INT_MAX;
+//                     for(int i=1; i<=3; i++){
+//                         if(currLane != i && obstacles[currPos] != i){
+//                             ans = min(ans, 1 + next[i]);
+//                         }
+//                     }
+//                     curr[currLane] = ans;
+//                 }
+//             }
+//             next = curr;
+//         }
+//         return min(next[2], min(1+next[1], 1+next[3]));
     }
 };
