619503d2b27482a7dc4e59df285adbec5cf0cd86
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2024-07-28 10:49:10 +0530

    Time: 1241 ms (56.24%), Space: 246.3 MB (50.65%) - LeetHub

---
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestArithSeqLength(vector<int>& arr) {
+        int n = arr.size();
+        int ans = 0;
+
+        unordered_map<int, int> dp[n+1];
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                int diff = arr[i] - arr[j];
+                int cnt = 1;
+
+                if(dp[j].count(diff)){
+                    cnt = dp[j][diff];
+                }
+
+                dp[i][diff] = 1 + cnt;
+                ans = max(ans, dp[i][diff]);
+            }
+        }
+        return ans;
+    }
+};
