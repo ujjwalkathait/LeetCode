27bd9a4a4f8f853fcf3ac3b6663774fe9f6f497c
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-04-01 22:04:40 +0530

    Time: 17 ms (11.61%) | Memory: 127.4 MB (36.85%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long solve(vector<vector<int>> &q, int i, int n, vector<long long> &dp){
+        if(i >= n){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        long long take = q[i][0] + solve(q, i+q[i][1]+1, n, dp);
+        long long notTake = solve(q, i+1, n, dp);
+        return dp[i] = max(take, notTake);
+    }
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        vector<long long> dp(n+1, -1);
+        return solve(questions, 0, n, dp);
+    }
+};
