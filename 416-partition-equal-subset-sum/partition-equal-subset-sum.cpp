a42009c63fbb044fcb70bf7d7445bf7b6fae4dd2
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-04-08 00:24:34 +0530

    Time: 191 ms (13.75%) | Memory: 186.2 MB (8.29%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,72 +1,32 @@
 class Solution {
 public:
-    int solve(int N, vector<int> arr, int target, int index, vector<vector<int>> &dp){
-        if(target < 0 || index >= N){
+    bool solve(vector<int> &nums, int i, int target, vector<vector<int>> &dp){
+        if(target < 0 || i >= nums.size()){
             return 0;
         }
         if(target == 0){
             return 1;
         }
-        if(dp[target][index] != -1){
-            return dp[target][index];
+        if(dp[i][target] != -1){
+            return dp[i][target];
         }
-        int take = solve(N, arr, target - arr[index], index+1, dp);
-        int notTake = solve(N, arr, target, index+1, dp);
-        
-        return dp[target][index] = take || notTake;
+        int take = solve(nums, i+1, target - nums[i], dp);
+        int notTake = solve(nums, i+1, target, dp);
+
+        return dp[i][target] = take | notTake;
     }
     bool canPartition(vector<int>& nums) {
-        int total = 0;
-        int N = nums.size();
-        for(int i=0; i<N; i++){
-            total += nums[i];
+        int n = nums.size();
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += nums[i];
         }
-        if(total % 2 == 1){
-            return 0;
+        if(sum % 2 == 1){
+            return false;
         }
-        int target = total / 2;
-        // vector<vector<int>> dp(target+1, vector<int>(N+1, -1));
-        // return solve(N, nums, target, 0, dp);
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));
+        int target = sum / 2;
 
-
-        // vector<vector<int>> dp(target+1, vector<int>(N+1, 0));
-        // for(int i=0; i<=N; i++){
-        //     dp[0][i] = 1;
-        // }
-        
-        // for(int t = 1; t<=target; t++){
-        //     for(int index = N-1; index>=0; index--){
-        //         int take = 0;
-        //         if(t - nums[index] >= 0){
-        //             take = dp[t - nums[index]][index+1];
-        //         }
-        //         int notTake = dp[t][index+1];
-                
-        //         dp[t][index] = take || notTake;
-        //     }
-        // }
-        // return dp[target][0];
-
-        vector<int> curr(target+1, 0);
-        vector<int> next(target+1, 0);
-        
-        
-        curr[0] = 1;
-        next[0] = 1;
-        
-        for(int index = N-1; index>=0; index--){
-            for(int t = 1; t<=target; t++){
-                int take = 0;
-                if(t - nums[index] >= 0){
-                    take = next[t - nums[index]];
-                }
-                int notTake = next[t];
-                
-                curr[t] = take || notTake;
-            }
-            next = curr;
-        }
-        return next[target];
-        
+        return solve(nums, 0, target, dp);
     }
 };
