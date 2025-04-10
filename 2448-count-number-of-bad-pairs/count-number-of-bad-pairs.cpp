e2b8d6587222f0ff5bc85b79193445f7379e724b
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-04-11 00:21:01 +0530

    Time: 70 ms (55.08%) | Memory: 92.6 MB (6.76%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -4,7 +4,6 @@ public:
         int n = nums.size();
         unordered_map<long long,int> mp;
         long long ans = 0;
-        // O(n)
         long long total = (long long)n*(n-1)/2;
         for(int j=0; j<n; j++){
             int value = mp[j-nums[j]];
@@ -15,11 +14,3 @@ public:
 
     }
 };
-/*
--4 -> 1 - 0
-0 -> 2 - 1
--1 -> 1 - 0
-[1,2,3,4,5]
-[0,1,2,3,4]
--1 -> 5 - 5*4/2 = 10
-*/
