c2dd188e5ba3b70e13f4ca072209b8e922d32e91
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-05-08 00:00:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (69.22%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,12 +5,13 @@ public:
         int i=0, j=0;
         int len = 0;
         while(j < s.size()){
-            mp[s[j]]++;
-            while(mp[s[j]] > 1){
-                mp[s[i]]--;
-                i++;
+            if(mp.find(s[j]) != mp.end()){
+                if(mp[s[j]] >= i){
+                    i = mp[s[j]]+1;
+                }
             }
             len = max(len, j-i+1);
+            mp[s[j]] = j;
             j++;
         }
         return len;
