2b192f60b5a9f568a7be8f590c69c563d53d495f
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-08-03 23:58:32 +0530

    Time: 115 ms (25.11%) | Memory: 174.4 MB (22.13%) - LeetSync

---
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int n = fruits.size();
+        int maxi = max({fruits.back()[0], k, startPos});
+        vector<int> arr(maxi+2, 0);
+        for (auto fruit : fruits) {
+            arr[fruit[0]] = fruit[1];
+        }
+        for (int i = 1; i <= maxi + 1; ++i) {
+            arr[i] += arr[i - 1];
+        }
+        int val = 0;
+        for(int i=0; i<=maxi + 1; i++){
+            if(arr[i] == 0)  arr[i] = val;
+            else  val = arr[i];
+        }
+        int ans = 0;
+        for(int x = 0; x <= k/2; x++){
+            int xi = startPos - x;
+            int yi = startPos + k - 2 * x;
+            if(yi > maxi) yi=maxi;
+
+            if(xi <= yi){
+                ans = max(ans, arr[yi] - (xi > 0 ? arr[xi - 1] : 0));
+            }
+
+            xi = startPos - (k - 2 * x);
+            yi = startPos + x;
+            if(yi > maxi) break;
+            if(xi <= yi){
+                ans = max(ans, arr[yi] - (xi > 0 ? arr[xi - 1] : 0));
+            }
+        }
+        return ans;
+    }
+};
