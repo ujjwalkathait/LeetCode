e32af0921127f73df0f7ef37a969bf8f96d84fef
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-08-23 00:50:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (33.97%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -17,24 +17,27 @@ public:
         // return solve(m-1, n-1, dp);
 
         vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = 1;
+        vector<int> prev(n, 0);
+        vector<int> next(n, 0);
+        prev[0] = 1;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(i == 0 && j == 0){
-                    dp[0][0] = 1;
+                    next[j] = 1;
                 }
                 else{
                     int up = 0, right = 0;
                     if(i > 0){
-                        up = dp[i-1][j];
+                        up = prev[j];
                     }
                     if(j > 0){
-                        right = dp[i][j-1];
+                        right = next[j-1];
                     }
-                    dp[i][j] = up + right;
+                    next[j] = up + right;
                 }
             }
+            prev = next;
         }
-        return dp[m-1][n-1];
+        return next[n-1];
     }
 };
