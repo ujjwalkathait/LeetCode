d025e1f128f4cf422f58a30513d3b75ddd81a52d
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-08-23 13:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (44.26%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -19,18 +19,21 @@ public:
         // return solve(0, 0, triangle, dp);
 
         vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<int> next(n, 0);
+        vector<int> curr(n, 0);
         for(int i=0; i<n; i++){
-            dp[n-1][i] = triangle[n-1][i];
+            next[i] = triangle[n-1][i];
         }
 
         for(int i=n-2; i>=0; i--){
             for(int j=i; j>=0; j--){
-                int down = triangle[i][j] + dp[i+1][j];
-                int diag = triangle[i][j] + dp[i+1][j+1];
+                int down = triangle[i][j] + next[j];
+                int diag = triangle[i][j] + next[j+1];
 
-                dp[i][j] = min(down, diag);
+                curr[j] = min(down, diag);
             }
+            next = curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
