142337f174876d7a9480306f6a868c60b01872d8
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-08-26 01:00:50 +0530

    Time: 47 ms (36.98%) | Memory: 48.8 MB (48.00%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -17,8 +17,26 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = solve(n-1, coins, amount, dp);
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
+        // int ans = solve(n-1, coins, amount, dp);
+        // if(ans >= 1e8) return -1;
+        // return ans;
+
+        for(int i=0; i<=amount; i++){
+            if(i % coins[0] == 0) dp[0][i] = i / coins[0];
+            else dp[0][i] = 1e8;
+        }
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=amount; j++){
+                int notTake = dp[i-1][j];
+                int take = INT_MAX;
+                if(coins[i] <= j){
+                    take = 1 + dp[i][j - coins[i]];
+                }
+                dp[i][j] = min(take, notTake);
+            }
+        }
+        int ans = dp[n-1][amount];
         if(ans >= 1e8) return -1;
         return ans;
     }
