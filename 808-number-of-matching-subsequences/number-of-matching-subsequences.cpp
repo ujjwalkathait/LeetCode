f0f5a43abb200549b105b52b633c08db4186941d
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-09-01 17:05:40 +0530

    Time: 287 ms (27.78%) | Memory: 342.2 MB (11.60%) - LeetSync

---
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool solve(string s, string t, int m, int n){
+        int j = 0;
+        for(int i = 0; i < m && j < n; i++){
+            if(s[i] == t[j]) j++;
+        }
+        return j == n;
+    }
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int ans=0;
+        map<string, bool> mp;
+        for(auto i : words){ 
+            if(mp.find(i) != mp.end()){
+                if(mp[i]){
+                    ans++;
+                }
+                continue;
+            }
+            mp[i] = solve(s ,i ,s.size() ,i.size());
+            ans += mp[i];
+        }
+        return ans;
+    }
+};
