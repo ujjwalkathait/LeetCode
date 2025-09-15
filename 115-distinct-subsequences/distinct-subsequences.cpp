0fc32fa9b8a1a55d3d8ca15b40fea82312cb5de1
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-09-15 22:09:01 +0530

    Time: 7 ms (96.65%) | Memory: 9 MB (86.39%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -22,14 +22,14 @@ public:
         for(int i=1; i<=n; i++){
             for(int j=m; j>=1; j--){
                 if(s[i-1] == t[j-1]){
-                    curr[j] = curr[j-1] + curr[j];
+                    prev[j] = prev[j-1] + prev[j];
                 }
                 else{
-                    curr[j] = curr[j];
+                    prev[j] = prev[j];
                 }
             }
         }
 
-        return (int)curr[m];
+        return (int)prev[m];
     }
 };
