bc794ed7477b61fbe74145c3934c9ff17cb98cf6
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-09-15 22:31:35 +0530

    Time: 9 ms (38.72%) | Memory: 13.2 MB (36.92%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -20,8 +20,30 @@ public:
         int n = word1.size();
         int m = word2.size();
 
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        // return solve(n-1, m-1, word1, word2, dp);
 
-        return solve(n-1, m-1, word1, word2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i=0; i<=n; i++){
+            dp[i][0] = i;
+        }
+        for(int j=0; j<=m; j++){
+            dp[0][j] = j;
+        }
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];            
+                }
+                else{
+                    int ins = 1 + dp[i][j-1];
+                    int del = 1 + dp[i-1][j];
+                    int rep = 1 + dp[i-1][j-1];
+
+                    dp[i][j] = min({ins, del, rep});
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
