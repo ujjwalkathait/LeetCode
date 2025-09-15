f90dc8f45898d4be753cff0cd3f80916263207a8
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-09-15 23:18:36 +0530

    Time: 20 ms (88.44%) | Memory: 12.9 MB (84.32%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,94 +1,55 @@
 class Solution {
 public:
-    bool solve(string s, string p, int i, int j, vector<vector<int>> &dp){
-        if(i==0 && j==0){
-            return true;
-        }
-        if(i>0 && j==0){
-            return false;
-        }
-        if(i==0 && j>0){
-            for(int k=1; k<=j; k++){
-                if(p[k-1] != '*'){
-                    return false;
-                }
+    bool solve(int i, int j, string p, string s, vector<vector<int>> &dp){
+        if(i < 0) return (j<0);
+        if(j < 0) {
+            for(int k=0; k<=i; k++){
+                if(p[k] != '*') return false;
             }
             return true;
         }
-
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        
-        if(s[i-1] == p[j-1] || p[j-1] == '?'){
-            return dp[i][j] = solve(s, p, i-1, j-1, dp);
+
+        if(p[i] == s[j] || p[i] == '?'){
+            return dp[i][j] = solve(i-1, j-1, p, s, dp);
         }
-        else if(p[j-1] == '*'){
-            return dp[i][j] = (solve(s, p, i, j-1, dp) || solve(s, p, i-1, j, dp));
+        if(p[i] == '*'){
+            bool ans = solve(i-1, j, p, s, dp) | solve(i, j-1, p, s, dp);
+            return dp[i][j] = ans;
         }
-        return false;
-    }
+        return dp[i][j] = false;
+    } 
     bool isMatch(string s, string p) {
-        // vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, -1));
-        // return solve(s, p, s.size(), p.size(), dp);
-
-        // vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, 0));
-        // dp[0][0] = 1;
-        // for(int j=1; j<=p.size(); j++){
-        //     bool flag = true;
-        //     for(int k=1; k<=j; k++){
-        //         if(p[k-1] != '*'){
-        //             flag = false;
-        //             break;
-        //         }
-        //     }
-        //     dp[0][j] = flag;
-        // }
-
-        // for(int i=1; i<=s.size(); i++){
-        //     for(int j=1; j<=p.size(); j++){
-        //         if(s[i-1] == p[j-1] || p[j-1] == '?'){
-        //             dp[i][j] = dp[i-1][j-1];
-        //         }
-        //         else if(p[j-1] == '*'){
-        //             dp[i][j] = (dp[i][j-1] || dp[i-1][j]);
-        //         }
-        //         else{
-        //             dp[i][j] = false;
-        //         }
-        //     }
-        // }
-        // return dp[s.size()][p.size()];
-
-        vector<int> curr(p.size()+1, 0);
-        vector<int> prev(p.size()+1, 0);
-        prev[0] = 1;
-        for(int j=1; j<=p.size(); j++){
+        int n = s.size();
+        int m = p.size();
+        
+        // return solve(m-1, n-1, p, s, dp);
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1, 0));
+        dp[0][0] = 1;
+        for(int i=1; i<=m; i++){
             bool flag = true;
-            for(int k=1; k<=j; k++){
+            for(int k=1; k<=i; k++){
                 if(p[k-1] != '*'){
                     flag = false;
+                    dp[i][0] = 0;
                     break;
-                }
+                } 
             }
-            prev[j] = flag;
+            if(flag) dp[i][0] = 1;
         }
-
-        for(int i=1; i<=s.size(); i++){
-            for(int j=1; j<=p.size(); j++){
-                if(s[i-1] == p[j-1] || p[j-1] == '?'){
-                    curr[j] = prev[j-1];
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(p[i-1] == s[j-1] || p[i-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
                 }
-                else if(p[j-1] == '*'){
-                    curr[j] = (curr[j-1] || prev[j]);
+                else if(p[i-1] == '*'){
+                    dp[i][j] = dp[i-1][j] | dp[i][j-1];
                 }
-                else{
-                    curr[j] = false;
-                }
+                else dp[i][j] = false;
             }
-            prev = curr;
         }
-        return prev[p.size()];
-
+        return dp[m][n];
     }
 };
