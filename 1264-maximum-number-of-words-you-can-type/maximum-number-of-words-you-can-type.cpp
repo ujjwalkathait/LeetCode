b326fbdb5f1129eba05a9b9d9bcba114724f0c14
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-09-15 22:07:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (57.81%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -7,30 +7,22 @@ public:
         for(auto x: bro){
             st.insert(x);
         }
-        //st = {l,t}
         int count = 0;
         for(int i = 0 ; i < text.size();i++){
             string curr="";
             while(i < text.size() && text[i] != ' '){
-                curr += text[i]; // curr = l
+                curr += text[i];
                 i++;
             }
             bool flag = true;    
             for(int k = 0; k < curr.size(); k++){
                 if(st.find(curr[k]) != st.end()) {
                     flag = false;
-                    break; //breaks
+                    break;
                 }
-            }//we will see
-            if(flag) count++;  //count =0 
-            //i got the sol wait i will write okay
+            }
+            if(flag) count++;
         }
         return count;
     }
 };
-//done reading the ques
-//starting from scratch what i can think of is we can like use a set to store each character of brokenletters to check whether a character of a word in text exists as a letter which we cannot type using this broken keyboard
-//please stop me if i am wrong, i will start with the code
-/* 
-
-*/
