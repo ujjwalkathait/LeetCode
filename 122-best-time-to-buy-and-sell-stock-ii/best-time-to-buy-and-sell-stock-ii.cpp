dedc4a960fb42f40dfdda100046d33ad4fa48839
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-09-18 00:33:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (86.84%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int solve(vector<int> &prices, int i, int buy, vector<vector<int>> &dp){
+        if(i >= prices.size()){
+            return 0;
+        }
+        if(dp[i][buy] != -1){
+            return dp[i][buy];
+        }
+        int profit = 0;
+        if(buy){
+            profit = max((-prices[i] + solve(prices, i+1, 0, dp)), (solve(prices, i+1, 1, dp)));
+        }
+        else{
+            profit = max((prices[i] + solve(prices, i+1, 1, dp)), (solve(prices, i+1, 0, dp)));
+        }
+        return dp[i][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        // vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        // return solve(prices, 0, 1, dp);
+
+        // vector<vector<int>> dp(prices.size()+1, vector<int>(2, 0));
+
+        // for(int i=prices.size()-1; i>=0; i--){
+        //     for(int buy=0; buy<=1; buy++){
+        //         int profit = 0;
+        //         if(buy){
+        //             profit = max((-prices[i] + dp[i+1][0]), (dp[i+1][1]));
+        //         }
+        //         else{
+        //             profit = max((prices[i] + dp[i+1][1]), (dp[i+1][0]));
+        //         }
+        //         dp[i][buy] = profit;
+        //     }
+        // }
+        // return dp[0][1];
+
+        vector<int> curr(2, 0);
+        vector<int> next(2, 0);
+
+        for(int i=prices.size()-1; i>=0; i--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy){
+                    profit = max((-prices[i] + next[0]), (next[1]));
+                }
+                else{
+                    profit = max((prices[i] + next[1]), (next[0]));
+                }
+                curr[buy] = profit;
+            }
+            next = curr;
+        }
+        return next[1];
+    }
+};
