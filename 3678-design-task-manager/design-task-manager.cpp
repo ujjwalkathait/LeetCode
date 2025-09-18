895affde19440869365951b6f9abda2ca61f89ce
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-09-18 22:42:32 +0530

    Time: 455 ms (22.81%) | Memory: 394.6 MB (22.19%) - LeetSync

---
+++ 3678-design-task-manager/design-task-manager.cpp
@@ -0,0 +1,58 @@
+class TaskManager {
+    unordered_map<int,pair<int,int>> taskToDetails;
+    unordered_map<int,int> taskToUser;// <taskId, userId>
+    set<pair<int,int>> pq; // <priority,taskid>
+    
+public:
+    TaskManager(vector<vector<int>>& tasks) {
+        for(int i = 0;i<tasks.size() ; i++){
+            taskToDetails[tasks[i][1]] = {tasks[i][2],tasks[i][0]};
+            taskToUser[tasks[i][1]] = tasks[i][0];
+            pq.insert({tasks[i][2],tasks[i][1]});
+        }
+    }
+    
+    void add(int userId, int taskId, int priority) {
+        taskToDetails[taskId] = {priority,userId};
+        taskToUser[taskId] = userId;
+        pq.insert({priority,taskId});
+    }
+    
+    void edit(int taskId, int newPriority) {
+        int prev = taskToDetails[taskId].first;
+        int user = taskToDetails[taskId].second;
+        taskToDetails[taskId] = {newPriority,user};
+        pq.erase({prev,taskId});
+        pq.insert({newPriority,taskId});
+        
+    }
+    
+    void rmv(int taskId) {
+        int priority = taskToDetails[taskId].first;
+        pq.erase({priority,taskId});
+        taskToDetails.erase(taskId);
+        taskToUser.erase(taskId);
+    }
+    
+    int execTop() {
+        if(pq.size() == 0) return -1;
+        pair<int, int> highest = *(pq.rbegin());
+        int user = taskToUser[highest.second];
+        rmv(highest.second);
+        return user; 
+    }
+};
+//{2,102},{4,103},{4,104}
+/*
+{
+    101 : {}
+}
+*/
+/**
+ * Your TaskManager object will be instantiated and called as such:
+ * TaskManager* obj = new TaskManager(tasks);
+ * obj->add(userId,taskId,priority);
+ * obj->edit(taskId,newPriority);
+ * obj->rmv(taskId);
+ * int param_4 = obj->execTop();
+ */
