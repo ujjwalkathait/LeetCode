21febfad2aee6c46131d82c8674d5633ef02a4f1
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-09-25 09:27:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (72.83%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -20,13 +20,13 @@ public:
         vector<int> curr(n + 1, 0);
         vector<int> next(n + 1, 0);
         for(int i=n-1; i>=0 ; i--){
-            for(int j=i; j>=0; j--){
-                int same = triangle[i][j] + next[j];
-                int nex = triangle[i][j] + next[j+1];
+            for(int j=0; j<=i; j++){
+                int same = triangle[i][j] + curr[j];
+                int nex = triangle[i][j] + curr[j+1];
 
                 curr[j] = min(same, nex);
             }
-            next = curr;
+            // next = curr;
         }
         return curr[0];
     }
