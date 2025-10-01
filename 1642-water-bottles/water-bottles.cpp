df43fab8f4f27a48ca1fa6cf03c6a3c1d7f5e183
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-10-01 22:56:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (45.09%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int ans = numBottles;
-        while(numBottles/numExchange >= 1){
-            ans += (numBottles/numExchange);
-            numBottles = (numBottles % numExchange) + (numBottles / numExchange);
-        }
-        return ans;
+        // int ans = numBottles;
+        // while(numBottles/numExchange >= 1){
+        //     ans += (numBottles/numExchange);
+        //     numBottles = (numBottles % numExchange) + (numBottles / numExchange);
+        // }
+        // return ans;
 
-        // return numBottles + (numBottles - 1) / (numExchange-1);
+        return numBottles + (numBottles - 1) / (numExchange-1);
     }
 };
