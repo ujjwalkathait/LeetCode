a04047b9a67c3e4389be9cf536942bb00e54fa5c
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-11-03 10:46:06 +0530

    Time: 2 ms (13.92%) | Memory: 13.5 MB (6.28%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &tri, int i, int j, vector<vector<int>> &dp){
-        if(i == tri.size() - 1){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int same = tri[i][j] + solve(tri, i+1, j, dp);
-        int next = tri[i][j] + solve(tri, i+1, j+1, dp);
+    int solve(vector<vector<int>> &triangle, int i, int j, vector<vector<int>> &dp){
+        if(i >= triangle.size()) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
 
-        return dp[i][j] = min(same, next);
+        int down = solve(triangle, i+1, j, dp);
+        int right = solve(triangle, i+1, j+1, dp);
+        return dp[i][j] = triangle[i][j] + min(down, right);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         // vector<vector<int>> dp(n, vector<int>(n, -1));
         // return solve(triangle, 0, 0, dp);
 
-        vector<int> curr(n + 1, 0);
-        vector<int> next(n + 1, 0);
-        for(int i=n-1; i>=0 ; i--){
-            for(int j=0; j<=i; j++){
-                int same = triangle[i][j] + curr[j];
-                int nex = triangle[i][j] + curr[j+1];
-
-                curr[j] = min(same, nex);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                int down = dp[i+1][j];
+                int right = dp[i+1][j+1];
+                dp[i][j] = triangle[i][j] + min(down, right);
             }
-            // next = curr;
         }
-        return curr[0];
+        return dp[0][0];
     }
 };
