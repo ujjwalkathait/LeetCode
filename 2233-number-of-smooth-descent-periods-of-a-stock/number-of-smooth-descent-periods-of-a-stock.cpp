8f48d755f67e5b8c3de945d85e4d1268426730cf
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2025-12-15 10:34:07 +0530

    Time: 0 ms (100.00%) | Memory: 106.9 MB (81.41%) - LeetSync

---
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long ans = 1;
+        int cnt = 1;
+        for(int i=1; i<prices.size(); i++){
+            if(prices[i] == prices[i-1] - 1) cnt++;
+            else cnt = 1;
+            ans += cnt;
+        }
+        return ans;
+    }
+};
