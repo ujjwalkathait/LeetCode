c3500ccc3218dc0c4eda2e30c6889221fb56cc91
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2026-01-09 14:54:30 +0530

    Time: 5 ms (37.90%) | Memory: 25.9 MB (28.91%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<TreeNode*, int> dp;
+    int dfs(TreeNode* root) {
+        if(!root) return 0;
+
+        if(dp.count(root)) return dp[root];
+
+        int take = root->val;
+        int notTake = dfs(root->left) + dfs(root->right);
+
+        if(root->left) take += dfs(root->left->left) + dfs(root->left->right);
+        if(root->right) take += dfs(root->right->left) + dfs(root->right->right);
+
+        return dp[root] = max(take, notTake);
+        
+    }
+    int rob(TreeNode* root) {
+
+        return dfs(root);
+    }
+};
