5f01028b03f5d737b0b458bc1c3bf6efe4b6a774
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2026-01-22 14:45:11 +0530

    Time: 36 ms (24.85%) | Memory: 139.1 MB (66.73%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -4,7 +4,7 @@ public:
         deque<int> dq;
         vector<int> ans;
         for(int i=0; i<nums.size(); i++){
-            if(!dq.empty() && dq.front() == i-k) dq.pop_front();
+            if(!dq.empty() && dq.front() <= i-k) dq.pop_front();
 
             while(!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
 
