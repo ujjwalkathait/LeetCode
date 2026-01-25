45e6fd3be066e2ef07f98bfc663b56bf6d14d1d2
ujjwalkathait <118869113+ujjwalkathait@users.noreply.github.com>
2026-01-25 22:06:52 +0530

    Time: 3 ms (19.42%) | Memory: 21.3 MB (88.58%) - LeetSync

---
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int cam = 0;
+    int dfs(TreeNode* root){
+        if(!root) return 1;
+
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+
+        if(left == 0 || right == 0){
+            cam++;
+            return 2;
+        }
+        else if(left == 2 || right == 2){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    int minCameraCover(TreeNode* root) {
+        int ans = dfs(root);
+        return ans == 0 ? cam+1 : cam;
+    }
+};
